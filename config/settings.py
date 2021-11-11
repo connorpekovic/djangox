df2b5084270497717bdecb554fd04fdbbe5b5fdb
connorpekovic <connorpeko@outlook.com>
2021-11-10 20:04:38 -0500

    settings.py allow subdomains

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [".herokuapp.com","localhost", "0.0.0.0", "127.0.0.1", "immense-forest-82676.herokuapp.com", "normalorg.com", "www.normalorg.com"]
+ALLOWED_HOSTS = [".herokuapp.com","localhost", "0.0.0.0", "127.0.0.1", "immense-forest-82676.herokuapp.com", ".normalorg.com", "www.normalorg.com" ]
 
 # APPS
 # ------------------------------------------------------------------------------
