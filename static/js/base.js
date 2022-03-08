7a0eaeb251d945c5b73cefb029cf9b823743cef9
connorpekovic <connorpeko@outlook.com>
2022-03-07 23:02:36 -0500

    Javascript improvements

--- static/js/base.js
+++ static/js/base.js
@@ -64,15 +64,31 @@ const chart1 = new Chart( context, config );
 
 /* Rest API Consumption example  */
 
+//Chicago Illinois
 let url_chicago = 'https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=a8efcfe69258b521961181dac55090ca'
-let tempChi = getTempature(url_chicago)
+let tempChi = await getTempature(url_chicago)
 console.log('Chicago is', tempChi)
 
 
-// Rest API call #2,   Lansing, MI
-let url_lansing = 'https://api.openweathermap.org/data/2.5/weather?q=lansing&appid=a8efcfe69258b521961181dac55090ca'
-let tempLansing = getTempature(url_lansing)
-console.log('Lansing is', tempChi)
+// Rest API call #2,   Dekalb, Illinois
+let url_Dekalb = 'https://api.openweathermap.org/data/2.5/weather?q=Dekalb&appid=a8efcfe69258b521961181dac55090ca'
+let tempDekalb = await getTempature(url_Dekalb)
+console.log('Dekalb is', tempDekalb)
+
+// Rest API call #2,   Manchester, TN
+let url_Manchester = 'https://api.openweathermap.org/data/2.5/weather?q=nashville&appid=a8efcfe69258b521961181dac55090ca'
+let tempManchester = await getTempature(url_Manchester)
+console.log('Manchester is', tempManchester)
+
+// Rest API call #3,   Bloomington IL
+let url_Bloomington = 'https://api.openweathermap.org/data/2.5/weather?q=bloomington&appid=a8efcfe69258b521961181dac55090ca'
+let tempBloomington = await getTempature(url_Bloomington)
+console.log('Bloomington is', tempBloomington)
+
+// Rest API call #3,   Okeechobee, FL
+let url_Okeechobee = 'https://api.openweathermap.org/data/2.5/weather?q=okeechobee&appid=a8efcfe69258b521961181dac55090ca'
+let tempOkeechobee = await getTempature(url_Okeechobee)
+console.log('Okeechobee is', tempOkeechobee)
 
 
 async function getTempature(url) {
@@ -93,6 +109,28 @@ async function getTempature(url) {
             return F  
         }
 } 
+
+async function getTeampature2(url) {
+    fetch(URL, {
+        headers:{
+            'Accept': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest', //Necessary to work with request.is_ajax()
+        },
+    })
+    .then(response => {
+        return response.json() //Convert response to JSON
+    })
+    .then(data => {
+        //Perform actions with the response data from the view
+        let K, F = 0
+        K = data.main.temp
+        F = ((K-273.15)*1.8)+32
+        F = Math.round(F)
+        console.log('Temperature is ', F)
+
+        return F  
+    })
+}
 
 
 //The context for RestAPI Consumer Chart
@@ -124,12 +162,14 @@ Chart objects are created with the Chart constructor. Ex: 'new Chart(context, co
 
 const context2 = document.getElementById('myChart2').getContext('2d');
 
-const labels2 = months({count: 7});
+console.log("Chicago is ", tempChi, ".  Dekalb temp is ", tempDekalb)
+
+const labels2 = ['Chicago, IL', 'Dekalb, IL',  'Manchester, TN', 'Bloomington, IL', 'Okeechobee, FL'];
 const setup2 = {
   labels: labels2,
   datasets: [{
     label: 'My First Dataset',
-    data: [tempChi, tempLansing, 80, 81, 56, 55, 40],
+    data: [tempChi, tempDekalb, tempManchester, tempBloomington, tempOkeechobee ],
     backgroundColor: [
       'rgba(255, 99, 132, 0.2)',
       'rgba(255, 159, 64, 0.2)',
