3dac71a937bffa3b54c40e601dd25ac4ed8fa682
Connor Pekovic <ConnorPekovic>
2022-12-11 21:41:18 -0500

    Moved code into js file

--- static/js/base.js
+++ static/js/base.js
@@ -1,4 +1,24 @@
 // import { LOCATIONS } from './utils.js'
+
+    /* jQuery for Google Places API location autocomplete. */
+    var searchInput = 'search_input';
+
+    $(document).ready(function () {
+        var autocomplete;
+        autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+            types: ['geocode'],
+            /*componentRestrictions: {
+            country: "USA"
+            }*/
+        });
+        
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var near_place = autocomplete.getPlace();
+        });
+    });
+
+
+
 /* 
     Display the local postgres data in a chart using Chart.js.
     1. Gather the data
